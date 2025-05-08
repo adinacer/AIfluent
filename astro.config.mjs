2d3eede3215401c2b27bcc7465bc09f9d87629b7
adinacer <0xsomesh@gmail.com>
2025-05-08 17:07:51 +0530

    package json changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aifluent.github.io',
-  base: '/landing',
+  site: 'https://aiflunce.github.io',
+  base: '/',
   integrations: [
     mdx(),
     tailwind(),
